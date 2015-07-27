a32d09b1ecf189b1ebe7afcf03616c1558f44ec3
Wush Wu <wush978@gmail.com>
2015-07-27 21:47:42 +0800

    update

--- update.R
+++ update.R
@@ -2,10 +2,18 @@ stopifnot(Sys.info()["sysname"] == "Darwin")
 
 library(miniCRAN)
 options(repos = "http://cran.csie.ntu.edu.tw")
-pkgs <- c("swirl", "dplyr", "ggplot2")
+pkgs <- c(
+  "swirl", 
+  "dplyr", 
+  "ggplot2", 
+  "gridExtra",
+  "reshape2",
+  "scales",
+  "data.table",
+  "sqldf")
 download <- TRUE
 pkgs.candidate <- pkgDep(pkgs)
-for(type in c("source", "win.binary", "mac.binary")) {
+for(type in c("source", "win.binary", "mac.binary", "mac.binary.mavericks")) {
   for(r.version in list(
     "3.1.1" = list(major="3", minor="1.1"), 
     "3.1.2" = list(major="3", minor="1.2"), 
@@ -14,41 +22,57 @@ for(type in c("source", "win.binary", "mac.binary")) {
     tmp <- R.version
     tmp$major <- r.version$major
     tmp$minor <- r.version$minor
-    pkgs.current <- pkgAvail(".", type = type, Rversion = paste(tmp$major, tmp$minor, sep = "."))
-    pkgs.avail <- available.packages(contrib.url(getOption("repos"), type = type))
-    pkgs.existed <- intersect(pkgs.candidate, rownames(pkgs.current))
-    pkgs.downloads <- c(
-      setdiff(pkgs.candidate, rownames(pkgs.current)),
-      pkgs.existed[which(package_version(pkgs.current[pkgs.existed, "Version"]) < package_version(pkgs.avail[pkgs.existed, "Version"]))])
-    makeRepo(pkgs.downloads, path = ".", download = download, type = type, Rversion = tmp)
+    pkgs.current <- try(pkgAvail("gh-pages", type = type, Rversion = paste(tmp$major, tmp$minor, sep = ".")), silent = TRUE)
+    if (class(pkgs.current) == "try-error") {
+      pkgs.downloads <- pkgs.candidate
+    } else {
+      pkgs.avail <- available.packages(contrib.url(getOption("repos"), type = type))
+      pkgs.existed <- intersect(pkgs.candidate, rownames(pkgs.current))
+      pkgs.downloads <- c(
+        setdiff(pkgs.candidate, rownames(pkgs.current)),
+        pkgs.existed[which(package_version(pkgs.current[pkgs.existed, "Version"]) < package_version(pkgs.avail[pkgs.existed, "Version"]))])      
+    }
+    makeRepo(pkgs.downloads, path = "gh-pages", download = download, type = type, Rversion = tmp)
   }
 }
 
-system("R CMD INSTALL -l /tmp swirl")
+system("R CMD build DSC2015Tutorial/DSC2015R")
+system("R CMD INSTALL -l /tmp DSC2015R_0.1.tar.gz")
+system("7z a DSC2015R_0.1.zip /tmp/DSC2015R")
 system("R CMD build swirl")
+system("R CMD INSTALL -l /tmp --build swirl_2.3.1.tar.gz")
 system("7z a swirl_2.3.1.zip /tmp/swirl")
-file.copy("swirl_2.3.1.tar.gz", "src/contrib/")
-file.copy("swirl_2.3.1.tgz", "bin/macosx/contrib/3.1")
-file.copy("swirl_2.3.1.tgz", "bin/macosx/contrib/3.2")
-file.copy("swirl_2.3.1.zip", "bin/windows/contrib/3.1")
-file.copy("swirl_2.3.1.zip", "bin/windows/contrib/3.2")
+for(pkg in c("swirl_2.3.1", "DSC2015R_0.1")) {
+  file.copy(sprintf("%s.tar.gz", pkg), "gh-pages/src/contrib/")
+  file.copy(sprintf("%s.tgz", pkg), "gh-pages/bin/macosx/contrib/3.1")
+  file.copy(sprintf("%s.tgz", pkg), "gh-pages/bin/macosx/contrib/3.2")
+  file.copy(sprintf("%s.tgz", pkg), "gh-pages/bin/macosx/mavericks/contrib/3.1")
+  file.copy(sprintf("%s.tgz", pkg), "gh-pages/bin/macosx/mavericks/contrib/3.2")
+  file.copy(sprintf("%s.zip", pkg), "gh-pages/bin/windows/contrib/3.1")
+  file.copy(sprintf("%s.zip", pkg), "gh-pages/bin/windows/contrib/3.2")  
+}
 
 current.wd <- getwd()
 path.list <- c(
-  "src/contrib",
-  "bin/macosx/contrib/3.1",
-  "bin/macosx/contrib/3.2",
-  "bin/windows/contrib/3.1",
-  "bin/windows/contrib/3.2"
+  "gh-pages/src/contrib",
+  "gh-pages/bin/macosx/contrib/3.1",
+  "gh-pages/bin/macosx/contrib/3.2",
+  "gh-pages/bin/macosx/mavericks/contrib/3.1",
+  "gh-pages/bin/macosx/mavericks/contrib/3.2",
+  "gh-pages/bin/windows/contrib/3.1",
+  "gh-pages/bin/windows/contrib/3.2"
   )
 type.list <- c(
   "source",
+  "mac.binary",
+  "mac.binary",
   "mac.binary",
   "mac.binary",
   "win.binary",
   "win.binary"
   )
 for(i in seq_along(path.list)) {
+  print(path.list[i])
   setwd(path.list[i])
   tools::write_PACKAGES(dir = ".", type = type.list[i])
   setwd(current.wd)
